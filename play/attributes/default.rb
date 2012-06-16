93dfd8259f7c19aa7520bd13c88f780550226255
Rich Kroll <kroll.rich@gmail.com>
2012-06-16 08:10:47 -0400

    Initial checkin of play framework cookbook.

---
+++ play/attributes/default.rb
@@ -0,0 +1,5 @@
+default[:play][:revision]                   = "HEAD"
+default[:play][:repository]                 = "https://github.com/playframework/play.git"
+
+default[:play][:path][:prefix]              = "/srv"
+default[:play][:path][:base]                = "#{default[:play][:path][:prefix]}/play"
---
+++ play/metadata.rb
@@ -0,0 +1,8 @@
+maintainer       "Rich Kroll"
+maintainer_email "kroll.rich@gmail.com"
+license          "Apache 2.0"
+description      "Installs Play!"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.2"
+recipe           "play", "Installs the Play Framework"
+depends          "java"
